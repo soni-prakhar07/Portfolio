03dcfa9f1f66f6049c3efb929b9de0159e9d8110
Prakhar Soni <soni.prakhar.04@gmail.com>
2025-02-08 13:37:11 +0530

    added Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import opinyo from "../../Assets/Projects/Opinyo.png";
 import orderease from "../../Assets/Projects/OrderEase.png";
+import cph from "../../Assets/Projects/CPH.png";
 
 function Projects() {
   return (
@@ -34,6 +35,15 @@ function Projects() {
               title="Opinyo"
               description="A customer feedback system that allows users to submit and view personalized feedback on products, with session-based authentication ensuring secure and unique user experiences. The system is optimized for easy deployment and management, providing insightful feedback tracking for businesses.  "
               ghLink="https://github.com/soni-prakhar07/Opinyo.git"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={cph}
+              isBlog={false}
+              title="CPH VScode Extension"
+              description="A VScode extension that allows users to easily fetch the test cases of a problem from Leetcode and run them on their local machine. The extension also provides multiple lanugage support such as C++, Python and Javascript."
+              ghLink="https://github.com/soni-prakhar07/CPH.git"
             />
           </Col>
         </Row>
